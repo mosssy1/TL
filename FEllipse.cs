03899ef4a1642e974019472c0181dfa53cc463a8
mosssy1 <99500509+mosssy1@users.noreply.github.com>
2022-02-17 00:10:55 +0300

    Add files via upload

---
+++ FEllipse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ellipse
+{
+    public class FEllipse
+    {
+        public point Centre;
+        public int HorizontalRadius { get; set; }
+        public int VerticalRadius { get; set; }
+        public FEllipse
+            (
+            point Centre,
+            int Horizontalradius,
+            int Verticalradius
+            )
+        {
+            if (Horizontalradius <= 0)
+            {
+                throw new ArgumentException("Горизонтальный радиус введён неверно");
+            }
+            if (Verticalradius <= 0)
+            {
+                throw new ArgumentException("Вертикальный радиус введён неверно");
+            }
+
+            Centre = Centre;
+            HorizontalRadius = Horizontalradius;
+            VerticalRadius = Verticalradius;
+
+        }
+        public double GetSquare()
+        {
+            return Math.Round(Math.PI * HorizontalRadius * VerticalRadius);
+        }
+        public double GetLenght()
+        {
+            return Math.Round(Math.PI * (HorizontalRadius + VerticalRadius));
+        }
+    }
+}
