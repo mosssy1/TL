56e0b9fbe92cc154b87bbd64499fab190d9f2d50
mosssy1 <99500509+mosssy1@users.noreply.github.com>
2022-02-22 22:09:51 +0300

    Add files via upload

---
+++ EllipseFigure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ellipse
+{
+    public class EllipseFigure
+    {
+        public point centre;
+        public int horizontalRadius { get; private set; }
+        public int verticalRadius { get; private set; }
+
+        public EllipseFigure(point centre, int horizontalradius, int verticalradius)
+        {
+            if (horizontalradius <= 0)
+            {
+                throw new ArgumentException("Горизонтальный радиус введён неверно");
+            }
+            if (verticalradius <= 0)
+            {
+                throw new ArgumentException("Вертикальный радиус введён неверно");
+            }
+
+            centre = centre;
+            horizontalRadius = horizontalradius;
+            verticalRadius = verticalradius;
+
+        }
+        public double GetSquare()
+        {
+            return Math.Round(Math.PI * horizontalRadius * verticalRadius);
+        }
+        public double GetLenght()
+        {
+            return Math.Round(Math.PI * (horizontalRadius + verticalRadius));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Ellipse;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        point centre = new point
+        {
+            X = 5,
+            Y = 5
+        };
+        int horizontalradius = 3;
+        int verticalradius = 4;
+        try
+        {
+            EllipseFigure ellipse = new EllipseFigure(centre, horizontalradius, verticalradius);
+     
+            Console.WriteLine($"Площадь эллипса -> {ellipse.GetSquare()}");
+            Console.WriteLine($"Длина дуги эллипса -> {ellipse.GetLenght()}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
