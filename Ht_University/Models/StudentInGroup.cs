04505175e4c5eeab35fd57bd1c7bf0f307029c7f
mosssy1 <99500509+mosssy1@users.noreply.github.com>
2022-03-16 14:12:31 +0300

    Add files via upload

---
+++ Ht_University/Models/StudentInGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ht_university.Models
+{
+   public class StudentInGroup
+    {
+        public int Id { get; set; }
+        public int GroupsId { get; set; }
+        public int StudentId { get; set; }
+    }
+}
